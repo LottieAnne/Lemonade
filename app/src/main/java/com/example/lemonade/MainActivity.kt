fbf5e9c8f05b9c53bc79e9338ab3c161527bf037
cchaae <kkang_chung125@yahoo.co.jp>
2023-02-14 22:35:39 +0900

    feat: クリックで画面切り替え

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,6 +12,10 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -32,37 +37,84 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun LemonApp() {
-    LemonTextAndImage()
+    var currentStep by remember { mutableStateOf(1) }
+
+    var squeezeCount by remember { mutableStateOf(0)}
+
+    Surface (
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ){
+        when (currentStep) {
+            1 -> {
+                LemonTextAndImage(
+                    textLabelResourceId = R.string.lemon_select,
+                    drawableResourceId = R.drawable.lemon_tree,
+                    contentDescriptionResourceId = R.string.lemon_tree_content_description,
+                    onImageClick = {
+                        currentStep = 2
+                    }
+                )
+            }
+            2 -> {
+                LemonTextAndImage(
+                    textLabelResourceId = R.string.lemon_squeeze,
+                    drawableResourceId = R.drawable.lemon_squeeze,
+                    contentDescriptionResourceId = R.string.lemon_content_description,
+                    onImageClick = {
+                        currentStep = 3
+                    }
+                )
+            }
+            3 -> {
+                LemonTextAndImage(
+                    textLabelResourceId = R.string.lemon_drink,
+                    drawableResourceId = R.drawable.lemon_drink,
+                    contentDescriptionResourceId = R.string.lemonade_content_description,
+                    onImageClick = {
+                        currentStep = 4
+                    }
+                )
+            }
+            4 -> {
+                LemonTextAndImage(
+                    textLabelResourceId = R.string.lemon_empty_glass,
+                    drawableResourceId = R.drawable.lemon_restart,
+                    contentDescriptionResourceId = R.string.empty_glass_content_description,
+                    onImageClick = {
+                        currentStep = 1
+                    }
+                )
+            }
+        }
+    }
 }
 
 @Composable
-fun LemonTextAndImage(modifier: Modifier = Modifier) {
+fun LemonTextAndImage(
+    textLabelResourceId: Int,
+    drawableResourceId: Int,
+    contentDescriptionResourceId: Int,
+    onImageClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
         modifier = modifier.fillMaxSize()
     ) {
         Text(
-            text = stringResource(R.string.lemon_select),
+            text = stringResource(textLabelResourceId),
             fontSize = 18.sp
         )
-        Image(painter = painterResource(R.drawable.lemon_tree),contentDescription = stringResource(R.string.lemon_tree_content_description))
-        Text(
-            text = stringResource(R.string.lemon_squeeze),
-            fontSize = 18.sp
+        Image(
+            painter = painterResource(drawableResourceId),
+            contentDescription = stringResource(contentDescriptionResourceId),
+            modifier = Modifier
+                .clickable(
+                    onClick = onImageClick
+                )
         )
-        Image(painter = painterResource(R.drawable.lemon_squeeze),contentDescription = stringResource(R.string.lemon_content_description))
-        Text(
-            text = stringResource(R.string.lemon_drink),
-            fontSize = 18.sp
-        )
-        Image(painter = painterResource(R.drawable.lemon_drink),contentDescription = stringResource(R.string.lemonade_content_description))
-        Text(
-            text = stringResource(R.string.lemon_empty_glass),
-            fontSize = 18.sp
-        )
-        Image(painter = painterResource(R.drawable.lemon_restart),contentDescription = stringResource(R.string.empty_glass_content_description))
-
     }
 }
 
